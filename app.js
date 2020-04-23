fee0bb68d7aefcd7e53504e8a336b6ed1035f6a2
schuckrw <55608123+schuckrw@users.noreply.github.com>
2020-04-23 12:24:54 -0700

    Initial Commit
    To establish a template and jumping of point.

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// To do: Connect MongoDB
+connectDB();
+
+
+// app.use is a middleware function (middleware is carried out in sequence)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'pug');
+
+// Routes (making app modular)
+const routes = require('./routes');
+
+// middleware for all routes
+app.use(routes);
+
+// Middleware (Testing middleware...)
+app.use((req, res, next) => {
+    console.log('Hello');
+    const err = new Error('This is not right!');
+    err.status = 500;
+    next(err);
+});
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+})
+
+// Error handler (work in progress) (takes 4 params) (return ...)
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    // Render an error template (pass in template file and the err object)
+    res.render('error', err);
+
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+});
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('To do: Main page')
+        // if(onLogIn) { res.render('mainPage) of {user} }
+        // else { res.redirect('landingPage') }
+});
+
+router.get('/landing', (req, res, ) => {
+    res.send('To do: Welcome Page (button to redirect to a register page and button to login page');
+    // if(onClick: register) { res.redirect('/register') }
+    // if(onClick: login) { res.redirect('/login') }
+    // else { res.send(err) }
+});
+
+router.get('/register', (req, res) => {
+    res.send('To do: Register Page w/ form for user creation');
+    // This route will render the page with form content
+});
+router.post('/register', (req, res) => {
+    // read in user input ---> assign to variables --> if(!exisist) { save to mongoDB(user.Schema) } 
+    // --> redirect to res.redirect('/landing') route
+});
+
+router.get('/login', (req, res) => {
+    res.send('To do: render login page');
+    // res.render('login')
+});
+router.post('/login', (req, res) => {
+    // read in user input ---> compare to mongoDB --> if(match) { start session for user } 
+    // --> redirect to res.redirect('/') route
+});
+
+router.get('/screen', (req, res) => {
+    res.send('To do: Route to render paitent screener form');
+    // res.render('screener')
+});
+router.post('/screen', (req, res) => {
+    // read in user input ---> assign to variables --> if(!err) { save to mongoDB(screen.Schema) } 
+    // --> redirect to res.redirect('/landing') route
+});
+
+module.exports = router;
