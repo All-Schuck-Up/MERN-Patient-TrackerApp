d0d010b822565196da960f875405f412c7b855ff
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2020-05-03 20:30:29 -0700

    Update App.js
    to show landingpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-import Title from './Components/Title';
+//import { Col, Button, Form, FormGroup, Label, Input } from "reactstrap";
+import Navbar from "./Components/Nbar.component";
+import createSympotom from "./Components/Create-patient-symptom.component";
+import login from "./Components/Login.component";
 import Landing from './Components/Landing';
+//"title" easy sample to be deleted
+import Title from './Components/Title';
 
 function App() {
   return (
-    <div className="App">
-                   <Title/>
-                   <Landing/>
-    </div>
+    <Router>
+        <div className="container">
+          <br/>
+          <Title />
+          <Landing />
+            
+        </div>
+    </Router>
   );
 }
 
