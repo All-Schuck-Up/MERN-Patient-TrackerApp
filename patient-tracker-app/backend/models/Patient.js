f2b7240bb2179d25ce04b7453a58326662d6ea71
schuckrw <55608123+schuckrw@users.noreply.github.com>
2020-05-03 20:43:18 -0700

    commit

---
+++ patient-tracker-app/backend/models/Patient.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const PatientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Patient = mongoose.model('patient', PatientSchema);
