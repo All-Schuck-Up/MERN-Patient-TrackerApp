ee1272d9d5a092302fea38542f034d843ce158cf
Asia <Joanna.gromadzka@seattlecentral.edu>
2020-05-05 23:57:14 -0700

    fix profile in nav

--- client/src/components/Nbar.component.js
+++ client/src/components/Nbar.component.js
@@ -11,7 +11,7 @@ export default class Navbar extends Component {
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
-          <Link to="/patient/profile" className="nav-link">Profile</Link>
+          <Link to="/patient/:id/profile" className="nav-link">Profile</Link>
           </li>
           <li className="navbar-item">
           <Link to="/login" className="nav-link">Logout</Link>
