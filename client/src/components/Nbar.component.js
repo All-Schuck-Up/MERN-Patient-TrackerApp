c17fa4c2f381400ece74c9902fa4b868994a16bd
Oyunomin Munkhkhurel <39477578+OyunominJuno@users.noreply.github.com>
2020-05-06 22:54:34 -0700

    Removed WelcomeProvider and changed Nav Bar
    for the consistency, Nav Bar now has props "name" for displaying whether it is patient or provider. WelcomeProvider is deleted and replaced by the Navbar

--- client/src/components/Nbar.component.js
+++ client/src/components/Nbar.component.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
       
       
       <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Welcome PATIENT NAME</Link>
+        <Link to="/" className="navbar-brand">Welcome {this.props.name}</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
-          <Link to="/patient/:id/profile" className="nav-link">Profile</Link>
+          <Link to="/patient/:id" className="nav-link">Go to Profile</Link>
           </li>
           <li className="navbar-item">
-          <Link to="/login" className="nav-link">Logout</Link>
+          <Link to="/logout" className="nav-link">Log Out</Link>
           </li>
          
         </ul>
