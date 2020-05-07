de8eb7f5519c1b257304acd1bf259e7c836cb88e
All-Schuck-Up <55608123+All-Schuck-Up@users.noreply.github.com>
2020-05-06 19:52:47 -0700

    update-routes
    added login functions

--- routes/patient/patient.js
+++ routes/patient/patient.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const Patient = require('../../models/Patient');
 
 router.get('/patient/login', (req, res) => {
 });
-router.post('/patient/login', (req, res) => {});
+// POST request: This route takes in user login input and compares it to database in order to login user. TEST WITH: => postman post to http://localhost:5000/patient/login
+router.post('/patient/login', [
+      check('email', 'Enter a valid email').isEmail(),
+      check('password', 'Password is required').exists(),
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      const { email, password } = req.body;
+  
+      try {
+        let patient = await Patient.findOne({ email });
+  
+        if (!patient) {
+          res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+
+        const isMatch = await bcrypt.compare(req.body.password, patient.password);
+
+        if(!isMatch) {
+            res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+
+        const payload = {
+            patient: {
+                id: patient.id
+            }
+        };
+        res.status(400).send('Logged in');
+
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+      }
+    }
+  );
 
 router.get('/patient/:_id', (req, res) => {
 });
@@ -42,7 +81,10 @@ router.post(
         password,
       });
 
-      // TO DO: --Possible password encryption
+      // encrypt password
+      patient.password = await bcrypt.hash(req.body.password, 10);
+      console.log(patient.password);
+      // End encryption
 
       await patient.save();
       res.send('Patient added');
--- routes/provider/provider.js
+++ routes/provider/provider.js
@@ -1,17 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 
 const Provider = require('../../models/Provider');
 
-router.get('/provider/login', (req, res) => { // TEST WITH: => http://localhost:3000/landing
-    //res.send('To do: render login page');
-    res.render('provider-login');
+router.get('/provider/login', (req, res) => { 
 });
-router.post('/provider/login', (req, res) => {
-    // read in user input ---> compare to mongoDB --> if(match) { start session for user } 
-    // --> redirect to res.redirect('/') route
-});
+// POST request: This route takes in user login input and compares it to database in order to login user. TEST WITH: => postman post to http://localhost:5000/provider/login
+router.post('/provider/login', [
+    check('email', 'Enter a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let provider = await Provider.findOne({ email });
+
+      if (!provider) {
+        res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(req.body.password, provider.password);
+
+      if(!isMatch) {
+          res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+          provider: {
+              id: provider.id
+          }
+      }
+      res.status(400).send('Logged in');
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // POST request, Add prover to database unless already exists.  TEST WITH: => POSTMAN POST to http://localhost:5000/provider/
 router.post('/provider/', [
@@ -40,7 +74,10 @@ router.post('/provider/', [
           password,
         });
   
-        // TO DO: --Possible password encryption
+       // encrypt password
+      provider.password = await bcrypt.hash(req.body.password, 10);
+      console.log(provider.password);
+      // End encryption
   
         await provider.save();
         res.send('Provider added');
