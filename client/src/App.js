4770ce9242e13b5791023dad8df4d6f493fce312
All-Schuck-Up <55608123+All-Schuck-Up@users.noreply.github.com>
2020-05-08 06:16:24 -0700

    INITIAL COMMIT

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,55 @@
-import React, { useState }from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
+import React, { useState, Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import { Col, Button, Form, FormGroup, Label, Input } from "reactstrap";
-import Landing from './components/Landing.component';
-import Login from "./components/Login.component";
-import LoginProvider from "./components/LoginProvider.component";
-import Navbar from "./components/Nbar.component";
-import CreateSymptom from "./components/Create-patient-symptom.component";
-import PatientProfile from "./components/PatientProfile.component";
+// import { Col, Button, Form, FormGroup, Label, Input } from "reactstrap"; (this is not being used)
+import Landing from './components/layout/Landing.component';
+import LoginPatient from './components/auth/LoginPatient.component';
+import LoginProvider from './components/auth/LoginProvider.component';
+import Navbar from './components/layout/Nbar.component';
+import CreateSymptom from './components/Create-patient-symptom.component';
+import PatientProfile from './components/PatientProfile.component';
 import WelcomeProvider from './components/WelcomeProvider.component';
 import PatientSearch from './components/PatientSearch.component';
 import PatientAlertList from './components/PatientAlertList.component';
 import PatientImmediateAttList from './components/PatientImmediateAttList.component';
 
-
 function App() {
   return (
     <Router>
-      <div>
-        <div className="container">
-          <br/>
-          <Route path="/">
+      <Fragment>
+        <Navbar />
+        <div>
+          <div className='container'>
+            <br />
+            <Route exact path='/'>
               <Landing />
-          </Route>
+            </Route>
+          </div>
+          <section className='container'>
+            <Switch>
+              <Route exact path='/patient/login' component={LoginPatient} />
+              <Route exact path='/provider/login' component={LoginProvider} />
+              <Route exact path='/patient/:id' component={CreateSymptom} />
+              <Route
+                exact
+                path='/patient/:id/profile'
+                component={PatientProfile}
+              />
+              <Route
+                exact
+                path='/provider/:id'
+                component={
+                  (WelcomeProvider,
+                  PatientSearch,
+                  PatientAlertList,
+                  PatientImmediateAttList)
+                }
+              />
+            </Switch>
+          </section>
         </div>
-      <div className="container">
-          <Route path="/patient/login">
-              <Login />
-          </Route>
-       </div>
-          <Route path="/provider/login">
-              <LoginProvider />
-          </Route>
-         <Route path="/patient/:id">
-              <Navbar />
-              <CreateSymptom />
-          </Route>
-        <Route path="/patient/:id/profile">
-              <Navbar />
-              <PatientProfile />
-          </Route>
-         <Route path="/provider/:id">
-              <Navbar />
-              <WelcomeProvider />
-              <PatientSearch />
-              <PatientAlertList />
-              <PatientImmediateAttList />
-          </Route>
-            
-        </div>
-     
+      </Fragment>
     </Router>
   );
 }
--- client/src/components/PatientProfile.component.js
+++ client/src/components/PatientProfile.component.js
@@ -1,11 +1,10 @@
-import React, {Component} from 'react';
-import Navbar from "./Nbar.component";
+import React, { Component } from 'react';
+import Navbar from './layout/Nbar.component';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-
 //symptoms delte to be change for update
-const PatientEntry = props => (
+const PatientEntry = (props) => (
   <tr>
     <td>{props.patientEntry.patientName}</td>
     <td>{props.patientEntry.form}</td>
@@ -14,38 +13,48 @@ const PatientEntry = props => (
     <td>{props.patientEntry.immediateAttention}</td>
     <td>{props.patientEntry.entryDate}</td>
     <td>
-      <Link to={"/edit/"+props.patientEntry._id}>edit</Link> | <a href="#" onClick={() => { props.deletePatientEntry(props.patientEntry._id) }}>delete</a>
+      <Link to={'/edit/' + props.patientEntry._id}>edit</Link> |{' '}
+      <a
+        href='#'
+        onClick={() => {
+          props.deletePatientEntry(props.patientEntry._id);
+        }}
+      >
+        delete
+      </a>
     </td>
   </tr>
-)
+);
 
-class PatientProfile extends Component{  
-    constructor(props) {
+class PatientProfile extends Component {
+  constructor(props) {
     super(props);
-        this.state = {patientEntries: []};
-    }
-     componentDidMount() {
-    axios.get('http://localhost:5000/patient/:id/profile')
-      .then(response => {
-        this.setState({ patientEntries: response.data })
+    this.state = { patientEntries: [] };
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/patient/:id/profile')
+      .then((response) => {
+        this.setState({ patientEntries: response.data });
       })
       .catch((error) => {
         console.log(error);
-      })
+      });
   }
-    patientEntryList() {
-    return this.state.patientEntries.map(currententry => {
-      return <PatientEntry patientEntry={currententry} key={currententry._id}/>;
-    })
+  patientEntryList() {
+    return this.state.patientEntries.map((currententry) => {
+      return (
+        <PatientEntry patientEntry={currententry} key={currententry._id} />
+      );
+    });
   }
-    render(){   
-       
-        return(
-             <div class = "container">
-            <h1>Profile</h1>
-            <h3>History</h3>
-            <table className="table">
-          <thead className="thead-light">
+  render() {
+    return (
+      <div class='container'>
+        <h1>Profile</h1>
+        <h3>History</h3>
+        <table className='table'>
+          <thead className='thead-light'>
             <tr>
               <th>patientName</th>
               <th>form</th>
@@ -54,12 +63,10 @@ class PatientProfile extends Component{
               <th>entryDate</th>
             </tr>
           </thead>
-          <tbody>
-            { this.patientEntryList() }
-          </tbody>
+          <tbody>{this.patientEntryList()}</tbody>
         </table>
-               </div>
-              )
-    }
-}                
+      </div>
+    );
+  }
+}
 export default PatientProfile;
