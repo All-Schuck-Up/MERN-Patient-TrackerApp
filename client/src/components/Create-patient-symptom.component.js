5560acf70df8e167278c5eeb3ba198823e26bb7a
yergu <56776980+yergu@users.noreply.github.com>
2020-05-10 15:08:52 -0700

    I add inputs  and  button functionality , and change form layout
    I change patient symptome-component
    I add file  upload   and other input
    Mark entry as immediate attention  Button ( on click event it change the the  value of  boolean variable  "attention "  to true initally it was fals
    put the reactStrap Card  to devide the page in to to section to view the Doctor note and to enter symptom

--- client/src/components/Create-patient-symptom.component.js
+++ client/src/components/Create-patient-symptom.component.js
@@ -1,117 +1,207 @@
 import React, { Component } from 'react';
+import axios from 'axios'; 
 
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import PatientProfile from "./PatientProfile.component";
 export default class createSympotom extends Component 
 {
   constructor(props)
   {
     super(props);
-    
-      this.onCheckedSymptom1=this.onCheckedSymptom1.bind(this);
+    this.onChangeDate=this.onChangeDate.bind(this);
+     this.onCheckedSymptom1=this.onCheckedSymptom1.bind(this);
      this.onCheckedSymptom2=this.onCheckedSymptom2.bind(this);
      this.onCheckedSymptom3=this.onCheckedSymptom3.bind(this);
      this.onCheckedSymptom4=this.onCheckedSymptom4.bind(this);
-     this.onChangeMedia=this.onChangeMedia.bind(this);
+     this.onChangeTemperature=this.onChangeTemperature.bind(this);
+     this.onMediaChange=this.onMediaChange.bind(this);
      this.onChangeadditionalNote=this.onChangeadditionalNote.bind(this);
-    // this.onChangeDate=this.onChangeDate.bind(this);
+ 
+    this.onChangeAttention=this.onChangeAttention.bind(this)
     this.onSubmit = this.onSubmit.bind(this);
-
+ // preserve the initial state in a new object
+    this.baseState = this.state 
     this.state ={
       date: Date().toLocaleString(),
       symptom1: '',
       symptom2: '',
       symptom3: '',
       symptom4: '',
-      additionalNote: '',
-      media: ''
+      temperature:'',
+      mediaDescription:'',
+      media:null,
+      attention:false,// onclick enable alert lable 
+      additionalNote: ''
+          
       // patientName
       //patientID
-
-
-    
-     // this.fileInput = React.createRef();
    
     }
+    
  
   }
-
- /* onChangeDate(e)
+  
+  onChangeDate(e)
   {
-    this.setState ({
+    this.setState ({ 
       date:e.target.value
       
     });
-  } */
+  };
+  componentDidMount() {
+
+    this.setState({ 
+
+      provider: ['test test'],
+
+      doctorNote: 'test Note'
+
+    });
+
+  }
+  /*
+  formReset(e) { 
+    this.setState({
+      symptom1: '',
+      symptom2: '',
+      symptom3: '',
+      symptom4: '',
+      temperature:'',
+      mediaDescription:'',
+      media:null,
+      attention:false,// onclick enable alert lable 
+      additionalNote: ''
+    });
+  };
+  */
+  onChangeAttention(e)
+  {
+    this.setState ({ 
+      attention:e.target.value
+      
+    });
+  };
+//on file select 
+
+onMediaChange = event => { 
+	
+	// Update the state 
+	this.setState({ media: event.target.files[0] }); 
+	
+	}; 
+	
+  //On file upload (click upload button)
+  onFileUpload(e)
+  {
+    //create an object of form data
+    const patientMedia= new FormData();
+    //update the formData object
+    patientMedia.append(
+      "myFile", 
+      this.state.media,
+      this.state.media.name
+      );
+      //file upload info
+//console.log(this.state.media);
+axios.post("http://localhost:3000/uploadfile", patientMedia); 
+ 
+  };
   onCheckedSymptom1(e) 
   {
     this.setState({
       sympetom1:e.target.value
       
     });
-  }
+  };
+
   onCheckedSymptom2(e)
   {
     this.setState({
       symptom2:e.target.value
       
     });
-  }
+  };
   onCheckedSymptom3(e)
   {
     this.setState({
       symptom3:e.target.checked
       
     });
-  }
+  };
   onCheckedSymptom4(e)
   {
     this.setState({
       symptom4:e.target.checked
       
     });
-  }
+  };
   onChangeadditionalNote(e)
   {
     this.setState({
       additionalNote:e.target.value
       
     });
-  }
-  onChangeMedia(e)
+  };
+ /* onChangeMedia(e)
   {
     this.setState({
       media:e.target.value
       
     });
-  }
+  }; */
+  
+  onChangeTemperature(e)
+  {
+    this.setState({
+      temperature:e.target.value
+      
+    });
+  };
 
   onSubmit(e)
   {
     e.preventDefault();
- const symptom  ={
+const symptom  ={
 date:this.state.date,
 symptom1:this.state.symptom1,
 symptom2:this.state.symptom2,
 symptom3:this.state.symptom3,
 symptom4:this.state.symptom4,
-additionalNote:this.state.additionalNote,
-media:this.state.media
-    }
+temperature:this.state.temperature,
+media:this.state.media,
+additionalNote:this.state.additionalNote
 
+    }
+  
 console.log(symptom);
 window.location ='/';
   }
- 
+  // reset
+  
 
   render() {
     return (
       <div>
      
-        <form className="symptomEntry" onSubmit={this.onSubmit}>
-        <h2 className="text-center">Patient Symptom Entery</h2>
-        <p>Date : {this.state.date.substring(0,21)}</p>
+     <Row>
+      <Col sm="6">
+        <Card body>
         
+          
+        <form className="symptomEntry" class="form-horizontal" onSubmit={this.onSubmit}>
+        <h3 className="text-center">Patient Symptom Entery</h3>
+        
+        <input  type ="hidden"
+                className="form-control"
+                value={this.state.attention}
+                onChange={this.onChangeAttention}
+                            />
+        <p>Date : {this.state.date.substring(0,21)}</p>
+    
+           
+            
     <div className="form-group" > 
-        <div> <label>Symptome 1 Question  </label> 
+        <div> <label>Trouble breathing ?  </label>{"      "} 
         
           
         <input class ="spaceInput" type="radio"
@@ -133,7 +223,7 @@ window.location ='/';
     </div>
     </div>
     <div className="form-group" > 
-        <div> <label>Symptome 2 Question </label>
+        <div> <label>Sore throat  ? </label> {"        "} 
         <label class="radio-inline">
           
         <input class ="spaceInput" type="radio" 
@@ -155,7 +245,7 @@ window.location ='/';
     </div>
    </div>
     <div className="form-group"> 
-        <div> <label>Symptome 3 Question </label> 
+        <div> <label>A dry cough ? </label> {"        "} 
         <label>
           
         <input class ="spaceInput" type="radio" 
@@ -175,7 +265,7 @@ window.location ='/';
       </label>
     </div> </div>
     <div className="formm-group">
-        <div> <label >Symptome 4 Question </label>
+        <div> <label >Heigh Fever ? </label>{"        "} 
         <label>
           
         <input  class ="spaceInput" type="radio" 
@@ -198,10 +288,37 @@ window.location ='/';
       </div>
     </div>
     
-  
+    <div className="form-group" > 
+        
+    <label class="control-label col-sm-2" >Temerature: </label>
+    
+             <input class ="spaceInput"  type ="text"
+                required
+                className="form-control"
+                value={this.state.temperature}
+                onChange={this.onChangeTemperature}
+                placeholder ={"body temperature "}
+                />
+          </div>
+       
+      
+      <div class="form-group">
+             
+     <div>  <label  >Media  : </label></div>
+      
+            <div> 
+                <input class ="spaceInput" 
+                 type="file"
+                 onChange={this.onMediaChange} /> 
+                <button onClick={this.onFileUpload}> 
+                  </button> 
+            </div> 
+         
+        </div> 
+
     <div className="form-group" > 
           <div>  <label>Additional Note : </label></div>
-            <textarea
+            <textarea class ="spaceInput" 
                 required
                 rows ={6}
 
@@ -212,22 +329,38 @@ window.location ='/';
                 />
           </div>
           
-          
-        
-  
           <div className="form-group">
+       
             <input type="submit" value="Save Record" className="btn btn-primary" />
           {" "}
-            <input type="submit" value="Mark entry as immediate attention" className="btn btn-secondary" />
+            
+            <button className="btn btn-secondary" onClick={() => this.setState({ attention: true})}>Mark entry as immediate attention</button>
          
           {" "}
-            <input type="submit" value="Cancle" className="btn btn-primary" />
+          <button   className="btn btn-primary" 
+          type="button">Cancel</button>
+          
          </div>
 
 
         </form>
+      
+        </Card>
+      </Col>
+      <Col sm="4">
+        <Card >
+          <CardTitle > <h3 className="text-center">Latest Doctor Note</h3></CardTitle>
+          <CardText> On click doctors note from patient profile will be displayed
+          On click doctors note from patient profile 
+          </CardText>
+          <Button >View  </Button>
+          
+        </Card>
+      </Col>
+    </Row>
       </div>
     )
   }
 }
+
 
