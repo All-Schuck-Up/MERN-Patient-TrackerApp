30a8a325b02fbb8c7aa42f2a1b0691eb0abc673f
yergu <56776980+yergu@users.noreply.github.com>
2020-06-02 22:39:13 -0700

    Junos route

--- routes/patient/patientEntry.js
+++ routes/patient/patientEntry.js
@@ -1,35 +1,15 @@
 const router = require('express').Router();
 let PatientEntry = require('../../models/Patient');
-const { check, validationResult } = require('express-validator');
-//fiel Upload beg
-let multer = require('multer');
-//et uidv4 = require('uuid/v4');
-const DIR = './public/';
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, DIR);
-    },
-    filename: (req, file, cb) => {
-        const fileName = file.originalname.toLowerCase().split(' ').join('-');
-        cb(null, uuidv4() + '-' + fileName)
-    }
+
+router.route('/patientEntries').get((req, res) => {
+ 
+    PatientEntry.find()
+        .then(patientEntries => res.json(patientEntries.patientEntry))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
-var upload = multer({
-    storage: storage,
-    limits : {fileSize : 1000000},
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype == "video/MP4" ||file.mimetype == "image/png" ||file.mimetype =="video/avi"|| file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-            cb(null, true);
-        } else {
-            cb(null, false);
-            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-        }
-    }
-});
-//end
- router.route('/patientEntry/add/:id',upload.single('media')).put((req, res) => { //patient Id
+router.route('/patientEntry/add/:id').put((req, res) => { //patient Id
 
     const formElement = {
         symptom1 :req.body.symptom1,
@@ -39,12 +19,8 @@ var upload = multer({
         temp : req.body.temp,
         comment : req.body.comment,
         doctorNote : req.body.doctorNote,
-        immediateAttention : req.body.immediateAttention,
-        media: url + '/public/' + req.file.filename
-    
-    };
-    
-        PatientEntry.findById(req.params.id)
+        immediateAttention : req.body.immediateAttention};
+    PatientEntry.findById(req.params.id)
     .then(patientEntries => {
         patientEntries.patientEntry.push(formElement)
 
@@ -56,60 +32,35 @@ var upload = multer({
 
 });
 
+// router.route('/patientEntry/:id').get((req, res) => {
+//     PatientEntry.findById(req.params.id)
+//         .then(patientEntry => res.json(patientEntry))
+//         .catch(err => res.status(400).json('Error: ' + err));
+// });
 
- 
-/* router.route('/patientEntry/add/:id',upload.single('media')).put((req, res) => { //patient Id
-req.check('symptom1','Shouldbe Selected').notEmpty();
-req.check('temp', 'Has to be a bumber').isNumeric();
-req.session.errors=null;
 
+//update route for the patient to add a note to their entry entered in last 24 hours
 
-let errors = req.validationErrors();
-if (errors)
- {
-    res.render('create-reservation.component',
-        {
-            title: 'Add Symptom',
-
-            errors: errors
-        });
-} else 
- {
-
-    const formElement = {
-        symptom1 :req.body.symptom1,
-        symptom2 :req.body.symptom2,
-        symptom3 :req.body.symptom3,
-        symptom4:req.body.symptom4,
-        temp : req.body.temp,
-        comment : req.body.comment,
-        doctorNote : req.body.doctorNote,
-        immediateAttention : req.body.immediateAttention,
-        media: url + '/public/' + req.file.filename
-    
-    };
+/*
+router.route('/PatientEntry/update/:id').post((req, res) => {
     PatientEntry.findById(req.params.id)
-    .then(patientEntries => {
-        patientEntries.patientEntry.push(formElement)
+        .then(patientsymptom => {
+            patientsymptom.additionalNote = req.body.additionalNote;
 
-        patientEntries.save(function (err) {
-            if (err)
-             {
-                console.log(err);
-                return;
-            }
-            else 
-            {
-                req.flash('scuess', 'Table Reserved');
-                res.redirect('/');
-            }
+            patientsymptom.save()
+                .then(() => res.json('Patient Entry Node  updated!'))
 
-        });
-        
+router.route('/patientEntry/update/:id').put((req, res) => {
+    PatientEntry.findById(req.params.id)
+        .then(entry => {
+            //entry.form[form.size() - 1[5]] = req.body.updatedAdditionalNote;
+            entry.doctorNote = req.body.doctorNote;
+            entry.save()
+                .then(() => res.json('Patient Entry Note Updated!'))
 
-    })
-  //  .catch(err => res.status(400).json('Error: ' + err));
- }
-}); */
-
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+*/
 module.exports = router;
