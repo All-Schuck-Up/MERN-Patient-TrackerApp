464719397b71161c1b0f284881d8dcba766f4dc3
Asia <Joanna.gromadzka@seattlecentral.edu>
2020-06-06 17:37:55 -0700

    it was already approved on previous branch

--- client/src/components/PatientProfile.component.js
+++ client/src/components/PatientProfile.component.js
@@ -1,67 +1,88 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-//import PatientEntries from './PatientEntryList';
+import CreateSymptoms from './CreateSymptoms.component';
 
-//assigned doctor
-//symptoms delete to be change for update
 const PatientEntry = props => (
   <tr>
-    <td>{props.patientEntry.patientName}</td>
     <td>{props.patientEntry.form}</td>
     <td>{props.patientEntry.additionalNote}</td>
     <td>{props.patientEntry.doctorNote}</td>
     <td>{props.patientEntry.immediateAttention}</td>
     <td>{props.patientEntry.entryDate}</td>
     <td>
-      <Link to={"/edit/"+props.patientEntry._id}>edit</Link> | <a href="/" onClick={() => { props.deletePatientEntry(props.patientEntry._id) }}>delete</a>
+      <Link to={"/edit/"+props.patientEntry._id}>edit</Link> | <a href="#" onClick={() => { props.deletePatientEntry(props.patientEntry._id) }}>delete</a>
     </td>
   </tr>
 )
 
-class PatientProfile extends Component{  
-    constructor(props) {
+export default class PatientProfile extends Component{ 
+   constructor(props){
     super(props);
-        this.state = {patientEntries: []};
+    this.state = {
+        assignedDoctor:'',
+        age:'',
+        email:'',
+        patient:[],
+        patientEntry:[],
+        patientname:'',
+        underlying:true,
+        patientId:''
     }
-     componentDidMount() {
-    axios.get('http://localhost:5000/patient/:id/profile')
-      .then(response => {
-        this.setState({ patientEntries: response.data })
+   }
+    
+    
+componentDidMount(){
+     axios.get('http://localhost:5000/patient/5ecaabd07dfcc538bce811fc')
+        .then(res => {
+            console.log(res);
+           
+         this.setState({
+                        patient: res.data.assignedDoctor,  
+                        patientname:(res.data.firstName + " "+ res.data.lastName),
+                        age: res.data.age,
+                        underlying: res.data.underlying
+         });
+        })
+        .catch((error) => {
+            console.log(error);
       })
-      .catch((error) => {
-        console.log(error);
-      })
-  }
-    patientEntryList() {
-    return this.state.patientEntries.map(currententry => {
-      return <PatientEntry patientEntry={currententry} key={currententry._id}/>;
-    })
-  }
-    render(){   
-       
-        return(
-             <div className = "container">
-            <h1>Profile</h1> 
-            <h3>Assigned Doctor:</h3>
-            <h2>History</h2>
-            <table className="table">
-          <thead className="thead-light">
-            <tr>
-              <th>patientName</th>
-              <th>entryDate</th>
-              <th>form</th>
-              <th>additionalNote</th>
-              <th>doctorNote</th>
-              <th>doctorNoteDate</th>
-            </tr>
-          </thead>
-          <tbody>
-            { this.patientEntryList() }
-          </tbody>
-        </table>
-               </div>
-              )
-    }
-}                
-export default PatientProfile;
+   
+  axios.get('http://localhost:5000/patientEntry/5ecaabd07dfcc538bce811fc')
+        .then(res => {
+            console.log(res);
+        this.setState({
+            patientEntry: res.data.map(el=>el.doctorNote)});
+        })
+        .catch((error) => {
+            console.log(error);
+      })   
+}
+
+render() {
+    const patientS=
+          <ul>{this.state.patientEntry.map((patientEntry) =>
+              <li key={patientEntry._id}>{patientEntry}
+              </li>
+              )}
+         </ul>
+
+    const patientP =
+            <ol>{this.state.patient}</ol>
+
+   
+    return(
+        <div className = "container">
+           <h1>Profile:</h1>
+           <h4>Patient name: <i>{this.state.patientname}</i>,
+           Age: <i>{this.state.age}</i>,  Underline condition: <i>{this.state.underlying.toString()}</i></h4> 
+           <h4>Assigned Doctor: <i>{patientP}</i></h4>    
+           <h2>History:</h2>
+               
+          <CreateSymptoms patientId='5ecaabd07dfcc538bce811fc'/> 
+             <div>Last Patient Note from old schema: {patientS}</div>
+        
+        </div>
+    )
+}
+}
